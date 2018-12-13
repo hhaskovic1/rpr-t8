4eac55fdc16b8a7a286d379e094a195dc4150329
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2018-12-13 09:26:11 +0100

    commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,38 +1,81 @@
 package sample;
 
+
+import javafx.application.Platform;
+import javafx.beans.Observable;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldListCell;
 
-import java.awt.*;
 import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
+    @FXML
+    private ListView<String> list;
+    @FXML
+    private TextField word = new TextField();
 
-    public TextField searchField;
-    public Button searchBtn;
-    SimpleStringProperty searchText;
+    @FXML
+    private Button dugme;
+    private File folder = new File("C:\\Users\\User");
+    private List<String> holder = new ArrayList<>();
 
-    public Controller(){
-        searchText = new SimpleStringProperty("");
+
+    public Button getSearch() {
+        return dugme;
     }
 
-    public SimpleStringProperty searchTextProperty(){
-        return searchText;
+    public void setSearch(Button search) {
+        this.dugme = search;
     }
 
-    public String getSearchText(){
-        return searchText.get();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
     @FXML
-    public void initialize(){
-        searchField.textProperty().bindBidirectional(searchText);
+    private void dugme(ActionEvent event) {
+        pretraga(word.getText(), folder);
+
+        for (String s : holder) {
+            list.getItems().add(s);
+        }
     }
 
-    public void btnPressed(ActionEvent actionEvent) {
 
+    public void pretraga(String word, File file) {
+        if (file == null) return;
+
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                if (files[i] == null) continue;
+                else if (files[i].getPath().contains(word)) {
+                    holder.add(files[i].getAbsolutePath());
+                } else {
+                    if (files[i].isDirectory())
+                        pretraga(word, files[i]);
+                }
+            }
+        }
+    }
+
+    public void wordAction(ActionEvent actionEvent) {
+    }
+
+    public void listAction(ListView.EditEvent<String> stringEditEvent) {
     }
 }
